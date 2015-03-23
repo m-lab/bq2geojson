e3aea458788a453ff3109cd1137d2910ef7a73b2
Nathan Kinkade <nath@nkinka.de>
2015-03-23 16:55:44 -0400

    Fix color selection, add metric filtering, add popup with metric data for each cell.

--- html/mlab.js
+++ html/mlab.js
@@ -6,12 +6,12 @@ function addLegend() {
 
 	legend.onAdd = function (map) {
 	    var div = L.DomUtil.create('div', 'info legend'),
-	        grades = [5, 10, 25, 50, 100];
+	        grades = [0, 5, 10, 25, 50];
 
 	    for ( var i = 0; i < grades.length; i++ ) {
 	        div.innerHTML +=
 	            '<i style="background:' + getHexColor(grades[i] + 1) + '"></i> ' +
-				(i == 0 ? '0' : grades[i-1])  + (grades[i + 1] ? '&ndash;' + grades[i] + ' mb/s<br>' : '+ mb/s');
+				(i == 0 ? '0' : grades[i])  + (grades[i + 1] ? '&ndash;' + grades[i + 1] + ' mb/s<br>' : '+ mb/s');
 	    }
 	    return div;
 	};
@@ -19,15 +19,15 @@ function addLegend() {
 
 }
 
-function getHexColor(avg) {
-    return avg > 100 ? 'blue' :
-           avg > 50  ? 'green' :
-           avg > 25  ? 'purple' :
-           avg > 10  ? 'yellow' :
-           avg > 5   ? 'red' : 'transparent';
+function getHexColor(val) {
+    return val > 50 ? 'blue' :
+           val > 25  ? 'green' :
+           val > 10  ? 'purple' :
+           val > 5  ? 'yellow' :
+           val > 0   ? 'red' : 'transparent';
 }
 
-function setHexLayer(url) {
+function getHexLayer(url, metric) {
 
 	document.getElementById('spinner').style.display = 'block';
 
@@ -35,7 +35,9 @@ function setHexLayer(url) {
 	xhr.onreadystatechange = function () {
 		if ( xhr.readyState === 4 ) {
 			if ( xhr.status === 200 ) {
-				addHexLayer(xhr.responseText);
+				setHexLayer(xhr.responseText, metric);
+			} else {
+				console.log(xhr.statusText);
 			}
 		}
 	}
@@ -44,31 +46,32 @@ function setHexLayer(url) {
 
 }
 
-function addHexLayer(geoJson) {
+function setHexLayer(geoJson, metric) {
 
 	var hexgrid = JSON.parse(geoJson);
 
 	hexgrid.features.forEach( function(cell) {
 
-		var download_avg = cell.properties.download_avg;
+		var value = cell.properties[metric];
 		var hexStyle = cell.hexStyle = {};
 
 		hexStyle.weight = 1;
 		hexStyle.fillOpacity = 0.3;
 
-		if ( ! download_avg ) {
+		if ( ! value ) {
 			hexStyle.weight = 0;
 			hexStyle.fillOpacity = 0;
 		} else {
-			hexStyle.color = getHexColor(download_avg);
+			hexStyle.color = getHexColor(value);
+			console.log('VAL: ' + value + '; COLOR: ' + hexStyle.color);
 		}
 
 	});
 
-	hexLayer = L.geoJson(hexgrid)
-		.eachLayer( function(l) {
-			l.setStyle(l.feature['hexStyle']);
-		});
+	hexLayer = L.geoJson(hexgrid).eachLayer( function(l) {
+		l.bindPopup(make_popup(l.feature.properties));
+		l.setStyle(l.feature['hexStyle']);
+	});
 
 	document.getElementById('spinner').style.display = 'none';
 
@@ -76,7 +79,19 @@ function addHexLayer(geoJson) {
 
 }
 
-function updateHexLayer(url) {
+function make_popup(props) {
+
+	var popup = 'DL throughput: ' + Math.round(props.download_avg * 10) / 10 + '<br/>';
+	popup += 'DL data points: ' + Math.round(props.download_count * 10) / 10 + '<br/>';
+	popup += 'UL throughput: ' + Math.round(props.upload_avg * 10) / 10 + '<br/>';
+	popup += 'UL data points: ' + Math.round(props.upload_count * 10) / 10 + '<br/>';
+	popup += 'Average RTT: ' + Math.round(props.rtt_avg);
+
+	return popup;
+
+}
+
+function updateHexLayer(url, metric) {
 	map.removeLayer(hexLayer);
-	setHexLayer(url);
+	getHexLayer(url, metric);
 }
