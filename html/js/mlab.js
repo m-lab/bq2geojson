a7cf12b780a6250eb45ddd8373a9b9eafc85ec05
Chris Ritzo <critzo@opentechinstitute.org>
2015-08-20 14:45:20 -0400

    updated javascript function to show/hide 'other' ISP text box when 'other' is selected in the ISP drop down

--- html/js/mlab.js
+++ html/js/mlab.js
@@ -497,10 +497,10 @@ $(function() {
 		}
 	});
 });
-window.onload=function() {
-  var other = document.getElementById('other');
-  other.style.display = 'none';
-  document.collector.isp.onchange = function() {
-    other.style.display =(this.value=='other')? '' : 'none';
-  };
-};
+function showOtherIspBox(val) {
+  var element=document.getElementById('isp');
+  if(val=='other')
+    element.style.display='block';
+  else
+    element.style.display='none';
+} 
