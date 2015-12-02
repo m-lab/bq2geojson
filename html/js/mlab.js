8549154f01e2c3dc09501899fd5cdc6e05229749
Chris Ritzo <critzo@opentechinstitute.org>
2015-12-02 12:55:15 -0500

    working on adjusting css for embedded iframe display

--- html/js/mlab.js
+++ html/js/mlab.js
@@ -8,16 +8,17 @@ function addLegend() {
 
 	legend.onAdd = function(map) {
 	    var div = L.DomUtil.create('div', 'info legend'),
-	        grades = [0, 5, 10, 25, 50];
+	        grades = [50, 25, 10, 5, 0];
 
-		div.innerHTML = '<i style="background: black; opacity: .2">' +
-			'</i>Insuff. data<br/>';
-	    for ( var i = 0; i < grades.length; i++ ) {
+		div.innerHTML = '';
+	    for ( var i = 50; i > grades.length; i-- ) {
 	        div.innerHTML +=
-	            '<i style="background:' + getPolygonColor(grades[i] + 1) +
-				'"></i> ' + (i == 0 ? '0' : grades[i]) + (grades[i + 1] ?
-				'&ndash;' + grades[i + 1] + ' Mbps<br/>' : '+ Mbps');
+	            '<i style="background:' + getPolygonColor(grades[i] - 1) +
+				'"></i> ' + (i == 0 ? '0' : grades[i]) - (grades[i - 1] ?
+				'&ndash;' + grades[i - 1] + ' Mbps<br/>' : '+ Mbps');
 	    }
+	    div.innerHTML += '<i style="background: black; opacity: .2">' +
+			'</i>Insuff. data<br/>';
 	    return div;
 	};
 	legend.addTo(map);
