b5a59490749807bcdcf0bd51c93d9836c5a442c1
Chris Ritzo <critzo@opentechinstitute.org>
2015-12-03 19:26:05 -0500

    first attempt at reversing legend order

--- html/js/mlab.js
+++ html/js/mlab.js
@@ -10,8 +10,18 @@ function addLegend() {
 	    var div = L.DomUtil.create('div', 'info legend'),
 	        grades = [0, 5, 10, 25, 50];
 
-		div.innerHTML = '<i style="background: black; opacity: .2">' +
-			'</i>Insuff. data<br/>';
+		div.innerHTML = '';
+	    for ( var i = grades.length - 1; i >= 0; i-- ) {
+	        div.innerHTML +=
+	            '<i style="background:' + getPolygonColor(grades[i] - 1) +
+				'"></i> ' + (i == 0 ? '0' : grades[i]) + (grades[i - 1] ?
+				'&ndash;' + grades[i - 1] + ' Mbps<br/>' : '+ Mbps');
+	    }
+		div.innerHTML + '<i style="background: black; opacity: .2">' +
+		'</i>Insuff. data';
+	    return div;
+
+/*
 	    for ( var i = 0; i < grades.length; i++ ) {
 	        div.innerHTML +=
 	            '<i style="background:' + getPolygonColor(grades[i] + 1) +
@@ -20,6 +30,7 @@ function addLegend() {
 	    }
 	    return div;
 	};
+*/
 	legend.addTo(map);
 }
 
