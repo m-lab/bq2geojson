60a59e41f10efd7981bd2e71f14b4828baa75690
Chris Ritzo <critzo@opentechinstitute.org>
2016-01-20 17:05:45 -0500

    testing reversion to piecewise branch index.html

--- html/js/ndt_d3.js
+++ html/js/ndt_d3.js
@@ -176,7 +176,6 @@ NDTmeter.prototype.onfinish = function (passed_results) {
   document.getElementById('ndt-results').style.display = "block";
   document.getElementById('extra-data').style.display = "block";
   document.getElementById('approx-loc').style.display = "none";
-  document.getElementById('ndt-results').style.display = "none";
 
   var child = document.getElementById('ndt');
   document.getElementById('sidebar').removeChild(child);
