2797f3c0a95e09cba351e772547659d41f3c0ec7
Chris Ritzo <critzo@opentechinstitute.org>
2016-02-04 07:00:08 -0500

    testing to see all key:values in props

--- html/js/mlab.js
+++ html/js/mlab.js
@@ -422,8 +422,8 @@ function makePopup(props) {
 		' Most common (median): ' + Math.round(props.upload_median * 10) / 10 + ' Mbps <br/>' +
 		' Max: ' + props.upload_max + ' Mbps<br /><br />' +
 		'<strong>Average Round Trip Time:</strong> ' + Math.round(props.rtt_avg) + ' ms <br/>';
-		for(var key in properties) {
-		    popup += properties[key] + '<br />';
+		for(var key in props) {
+		    popup += props[key] + '<br />';
 		}
 	return popup;
 }
